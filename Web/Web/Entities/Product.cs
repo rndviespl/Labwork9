e47cdc29edd214a5b5afa7ff1d67eceaae9c5b8b
0109-03 <0109-03@0109-03>
2024-11-08 11:41:14 +0300

    РґРѕР±Р°РІР»РµРЅС‹ РєРѕРјРјРµРЅС‚Р°СЂРёРё РІ РєР»Р°СЃСЃ Product

--- Web/Web/Entities/Product.cs
+++ Web/Web/Entities/Product.cs
@@ -1,13 +1,33 @@
 ﻿namespace Web.Entities
 {
+    /// <summary>
+    /// Сущность товар
+    /// </summary>
     public class Product
     {
+        /// <summary>
+        /// уникальный идентификатор
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// название
+        /// </summary>
         public string Name { get; set; } = string.Empty;
 
+        /// <summary>
+        /// описание
+        /// </summary>
         public string Description { get; set; } = string.Empty;
 
+        /// <summary>
+        /// стоимость в рублях
+        /// </summary>
         public decimal Price { get; set; }
+
+        /// <summary>
+        /// вес
+        /// </summary>
+        public decimal Weight { get; set; }
     }
 }
--- Web/Web/Migrations/20241108083916_fix.Designer.cs
+++ Web/Web/Migrations/20241108083916_fix.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Web.Data;
 
@@ -10,9 +11,11 @@ using Web.Data;
 namespace Web.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241108083916_fix")]
+    partial class fix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
